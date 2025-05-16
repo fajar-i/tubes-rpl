dce2b0a8732b31663852849a5c34a6b08631ba47
Bjari <fajariarkan@upi.edu>
2025-05-16 21:25:57 +0700

    p

--- backend/app/Http/Controllers/ProductController.php
+++ backend/app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use PhpParser\Node\Stmt\TryCatch;
+use Illuminate\Support\Facades\Storage;
 
 class ProductController extends Controller
 {
